ef923fc3cf9e076cb62c6a4f5a84962d84d185d3
Burban Maryan <burbanmaryan02@gmail.com>
2024-04-25 11:30:36 +0300

    setup project

---
+++ declaration.d.ts
@@ -0,0 +1,13 @@
+// styles
+declare module '*.scss';
+declare module '*.css';
+
+// assets
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.ico';
+declare module '*.webp';
+declare module '*.avif';
